5946514e0e1b0afe6d8132e007e11b901c444172
TrarbachArthur <arthurtrarbach.sampaio@gmail.com>
2023-06-29 01:18:28 -0300

    :beers: make grade graphs

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ app.title = 'Introcomp'
 
 text_style = {
     'textAlign': 'center',
-    'color': gp.colors['text']
+    'color': gp.colors['text'],
+    'marginBottom': 0
 }
 
 # CREATING PAGE STRUCTURE============================================================== #
 app.layout = html.Div(style={'backgroundColor': gp.colors['background']}, children=[
-    
+    html.H1(children='Análise de inscritos no Introcomp 2023', style=text_style),
+    html.H2(children='Escolaridade', style=text_style),
+    dcc.Graph(id='gg', figure=gp.grade_graph),
+    html.H3(children='Sabem programar, por escolaridade', style=text_style),
+    dcc.Graph(id='pbg', figure=gp.program_grade_graph)
+    html.H2(children='Genêro', style=text_style),
+    dcc.Graph(id='gg', figure=gp.grade_graph)
 ])
 
 if __name__ == '__main__':
--- modules/graphs.py
+++ modules/graphs.py
@@ -1,8 +1,40 @@
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 from modules.df_manage import extract_data_and_amount, filter_dataframe
 
+# ----- Registrations ----- #
+df = pd.read_csv("./data/Inscricoes2023.csv");
+
+# -------- Grade -------- #
+
+grade = df[df['escolaridade'].str.contains('2006') == False]
+escolaridade = grade['escolaridade'].value_counts()
+
+grade['programar'] = grade['sabe programar']
+program_by_grade = grade.pivot_table(values='programar', index=['sabe programar', 'escolaridade'], aggfunc='count').reset_index()
+
+# ----- Graphs ----- #
+grade_graph = px.pie(escolaridade, values='count', names=escolaridade.index)
+grade_graph.update_traces(textinfo='percent+value')
+
+program_grade_graph = px.histogram(program_by_grade, x='escolaridade', y='programar', color='sabe programar', color_discrete_sequence=['red', 'green'])
+program_grade_graph.update_layout(yaxis_title='Sabe programar', xaxis_title='')
+
+# ----- Editing graphs layouts ----- #
 colors = {
     'background': '#020220',
     'text': '#FFFFFF'
 }
+
+grade_graph.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+program_grade_graph.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
